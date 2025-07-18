acc0371a4c3c9030b9776380765ad6c04c6aeb50
Acgalbicsek <acgalbicsek@gmail.com>
2025-07-18 07:21:16 -0800

    stop tracking app settings

--- WelcomeWebpage/Controllers/WeatherController.cs
+++ WelcomeWebpage/Controllers/WeatherController.cs
@@ -9,10 +9,11 @@ namespace WelcomeWebpage.Controllers
     {
 
         private readonly IHttpClientFactory _httpClientFactory;
-
-        public WeatherController(IHttpClientFactory httpClientFactory)
+        private readonly string _apikey;
+        public WeatherController(IHttpClientFactory httpClientFactory, IConfiguration config)
         {
             _httpClientFactory = httpClientFactory;
+            _apikey = config["ApiSettings:ApiKey"];
         }
 
         public async Task<IActionResult> Index(string city = "Fairbanks")
@@ -24,8 +25,8 @@ namespace WelcomeWebpage.Controllers
         private async Task<Weather> GetWeatherAsync(string city)
         {
             var client = _httpClientFactory.CreateClient();
-            var apikey = "fc406fd5d170148b4195ae74e321da58";
-            var url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={apikey}";
+           
+            var url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={_apikey}";
 
             var response = await client.GetAsync(url);
             if (!response.IsSuccessStatusCode) return null;
